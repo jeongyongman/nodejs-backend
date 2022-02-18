0de8157dc8fbbec82400187e6c66e64a3fad3038
jeongyongman <i1683u@gmail.com>
2022-02-18 16:56:38 +0900

    회원가입 요청 구현

--- app/src/public/js/home/register.js
+++ app/src/public/js/home/register.js
@@ -4,30 +4,37 @@
 // console.log("hello");
 // console.log("bye");
 
-// login 기능 구현 - 프론트 단 기능 구현
+// register 기능 구현 - 프론트 단 기능 구현
 // 아이디와 비밀번호 입력 받고
 // 로그인 버튼이 눌릴 때 server로 전달이 된다
 // 아이디와 패스워드를 javascript로 처리해야 된다
 
 const id = document.querySelector("#id"),
+    name = document.querySelector("#name"),
     psword = document.querySelector("#psword"),
-    loginBtn = document.querySelector("#button");
+    confirmPsword = document.querySelector("#confirm-psword"),
+    registerBtn = document.querySelector("#button");
 
 // console.log(id);
 // console.log("hello");
+// console.log("hello register");              // ← 테스트
 
-loginBtn.addEventListener("click",login); // 두 번째 인수는 함수이다
+registerBtn.addEventListener("click", register); // 두 번째 인수는 함수이다
 
-function login(){
+function register(){
     // console.log("bye");
     // console.log(id.value);
     const req = {
         id: id.value,
+        name: name.value,         // ←
         psword: psword.value,
+        confirmPsword: confirmPsword.value,         // ←
     };
     // console.log(req); // 일반 req 리퀘스트 데이터와
     // console.log(JSON.stringify(req)); // JSON 형태로 감싼 req 리퀘스트 데이터 비교해 보자
-    fetch("/login", {
+    console.log(req);   // ← 테스트
+
+    fetch("/register", {
         method: "POST",
         headers: {
           "Content-Type": "application/json", // 내가 전달하는 데이터의 타입
@@ -37,13 +44,12 @@ function login(){
       .then((res) => res.json())        // ← 이 부분 수정하고
       .then((res) => {				// ← 여기 부분
         if(res.success){                                   // 개발하기쉽게 임시로 주석처리 20
-          location.href = "/";		// 이동할 링크           // 개발하기쉽게 임시로 주석처리 20
+          location.href = "/login";		// 이동할 링크           // 개발하기쉽게 임시로 주석처리 20
         } else {
           alert(res.msg);				// 서버에서 전달한 메시지// 개발하기쉽게 임시로 주석처리 20
         }                                                // 개발하기쉽게 임시로 주석처리 20
       })
       .catch((err) => {
-        console.error("로그인 중 에러 발생");
-        // console.error(new Error("로그인 중 에러 발생"));
+        console.error("회원가입 중 에러 발생");        // ← 이 부분 수정하고
       });
 };
